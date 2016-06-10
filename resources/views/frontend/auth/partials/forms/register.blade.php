2384c57fa86155c15f342339f197cbeaa2931230
Vincent Mimoun-Prat <vpratfr@gmail.com>
2016-06-10 21:54:27 +0200

    Registration layout + switch theme

--- resources/views/frontend/auth/partials/forms/register.blade.php
+++ resources/views/frontend/auth/partials/forms/register.blade.php
@@ -2,14 +2,9 @@
         ->action(route('frontend.auth.register.submit'))
         ->post() !!}
 
-<div class="row">
-    <div class="col-md-6">
-        {!! BootForm::text(trans('general.field.name'), 'name')  !!}
-    </div>
-    <div class="col-md-6">
-        {!! BootForm::email(trans('general.field.email'), 'email')  !!}
-    </div>
-</div>
+{!! BootForm::text(trans('general.field.name'), 'name')  !!}
+{!! BootForm::email(trans('general.field.email'), 'email')  !!}
+
 <div class="row">
     <div class="col-md-6">
         {!! BootForm::password(trans('general.field.password'), 'password')  !!}
@@ -18,7 +13,6 @@
         {!! BootForm::password(trans('general.field.password_confirmation'), 'password_confirmation')  !!}
     </div>
 </div>
-
 
 {!! BootForm::submit(trans('general.action.register'), 'btn btn-primary btn-block auth-submit') !!}
 
